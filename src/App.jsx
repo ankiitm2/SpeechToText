577d75f2aef9a70f580ffe7fcc552558e1076f2c
Ankit Mishra <ankit.mishra@paavu.com>
2024-02-09 16:22:39 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <div className="container m-8 flex flex-col m-auto mt-20">
         <h2 className="text-center font-medium text-3xl">Speech to Text</h2>
         <div className="main shadow text-2xl font-medium capitalize min-h-96 max-w-3xl w-full h-auto p-6 relative m-auto rounded-md my-2">
-          {transcript} hi
+          {transcript}
         </div>
         <div className="btn flex justify-around max-w-3xl w-full m-auto">
           <button className="bg-green-600 text-lg h-fit shadow-md text-white rounded-md">Copy</button>
