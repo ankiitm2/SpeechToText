32e0af5d828c4c272113fcb06cd6e770847057c3
Ankit Mishra <ankit.mishra@paavu.com>
2024-02-09 15:24:08 +0530

    first commit

--- src/openai.js
+++
@@ -1,3 +0,0 @@
-const { Configuration, OpenAIApi } = require('openai');
-const configuration = new Configuration({ apikey: "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" });
-const openai = new OpenAIApi(configuration);
